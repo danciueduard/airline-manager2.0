fc1e722d8f5e60a72341b9d60a51e27b0b31abcf
danciueduard <you@example.com>
2024-09-20 01:45:49 +0300

    fuel page from services was created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,6 @@ export class AppComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.router.navigate(["/staff"]);
+    this.router.navigate(["/services"]);
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { FleetComponent } from "./footer/menu/fleet/fleet.component";
 import { AirportsComponent } from "./footer/menu/airports/airports.component";
 import { StaffComponent } from "./footer/menu/staff/staff.component";
+import { ServicesMenuComponent } from "./footer/menu/services-menu/services-menu.component";
 
 export const routes: Routes = [
   { path: "fleet", component: FleetComponent },
   { path: "airports", component: AirportsComponent },
   { path: "staff", component: StaffComponent },
+  { path: "services", component: ServicesMenuComponent },
 ];
