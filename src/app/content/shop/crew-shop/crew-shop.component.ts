130bd5578bf573a6f049a8e8dd66b00825037921
danciueduard <you@example.com>
2024-09-30 15:50:55 +0300

    no com

---
+++ src/app/content/shop/crew-shop/crew-shop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crew-shop',
+  standalone: true,
+  imports: [],
+  templateUrl: './crew-shop.component.html',
+  styleUrl: './crew-shop.component.css'
+})
+export class CrewShopComponent {
+
+}
--- src/app/shared/supabase/supabase-services/supabase-store-service.service.ts
+++ src/app/shared/supabase/supabase-services/supabase-store-service.service.ts
@@ -9,13 +9,14 @@ import { SupabaseService } from "./supabase-client.service";
 export class SupabaseStoreService {
   constructor(private supabaseClientService: SupabaseService) {}
 
-  // I have used a BehaviorSubject to avoid useless re-fetching when accessing the store tabs
   private planesStoreSubject = new BehaviorSubject<PlaneModel[] | null>(null);
   public planes$ = this.planesStoreSubject.asObservable();
+  ///////////////////////////////////////////////////////////////////////////////
+  private crewStoreSubject = new BehaviorSubject<any | null>(null);
+  public crew$ = this.crewStoreSubject.asObservable();
 
   getPlanes(): Observable<PlaneModel[]> {
     const currentData = this.planesStoreSubject.getValue();
-
     if (currentData === null) {
       const data = this.supabaseClientService.getFromSupabase("planes", "*");
       from(data).subscribe((res) => {
@@ -23,5 +24,16 @@ export class SupabaseStoreService {
       });
     }
     return this.planes$;
+  }
+
+  getCrew(): Observable<any> {
+    const currentData = this.crewStoreSubject.getValue();
+    if (currentData === null) {
+      const data = this.supabaseClientService.getFromSupabase("workers", "*");
+      from(data).subscribe((res) => {
+        this.crewStoreSubject.next(res);
+      });
+    }
+    return this.crew$;
   }
 }
